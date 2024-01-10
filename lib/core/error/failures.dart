3223681ed0a0c73de7ee29ff09007f8c28deb206
Guilherme <gui.constantino@gmail.com>
2024-01-10 14:05:23 -0300

    Class 5

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -3,3 +3,24 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   const Failure([List properties = const <dynamic>[]]) : super();
 }
+
+// General failures
+class ServerFailure implements Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
+  @override
+  // TODO: implement stringify
+  bool? get stringify => false;
+}
+
+class CacheFailure implements Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
+  @override
+  // TODO: implement stringify
+  bool? get stringify => false;
+}
